b40a2b8c61cacb6eaaa5277e87172fba8fff77e9
mostafizz fuad <mostafizzfuad@gmail.com>
2023-02-09 12:27:14 +0600

    update code

--- assisn.php
+++ assisn.php
@@ -1,8 +1,4 @@
 
 <?php
 
-$tutionFee = 30000;
-
-$tutionFee = ($tutionFee>20000) ? "25 parcent commission" : (($tutionFee>10000) && ($tutionFee<20000)) ? "20 parcent commission" : (($tutionFee>7000) && ($tutionFee<10000)) ? "15 parcent commission" : "Data will be invalid";
-
-echo $tutionFee;
+echo "Hello BD";
