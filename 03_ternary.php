bf793ca4d80fc0b9f501748da39ce17f19ffba85
mostafizz fuad <mostafizzfuad@gmail.com>
2023-02-09 23:00:38 +0600

    update code

--- 03_ternary.php
+++ 03_ternary.php
@@ -5,6 +5,6 @@ $num1 = 5000;
 $num2 = 8000;
 $num3 = 7000;
 
-$largeNumber = (($num1 > $num2) ? $num1 : ($num2 > $num3)) ? $num2 : $num3;
+$largeNumber = ($num1 > $num2) ? $num1 : (($num2 > $num3) ? $num2 : $num3);
 echo $largeNumber;
 
